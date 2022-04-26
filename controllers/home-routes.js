2f1a6e9af1f342f5f241cc4ae7b7bd8a24afd727
sp381 <sarah.powell381@gmail.com>
2022-04-25 17:16:06 -0700

    added {{if}} statements

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -6,15 +6,8 @@ const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
   res.render('homepage', {
-      id: 1,
-      post_url: 'https://handlebarsjs.com/guide/',
-      title: 'Handlebars Docs',
-      created_at: new Date(),
-      vote_count: 10,
-      comments: [{}, {}],
-      user: {
-          username: 'test_user'
-      }
+    posts,
+    loggedIn: req.session.loggedIn
   });
 });
 
